01736970e7619028d3e7654bc3bf41a2549673a2
Luke Tseng <lukegtseng@gmail.com>
2018-12-14 10:37:14 -0800

    done

--- HelloGithub/src/Main.java
+++ HelloGithub/src/Main.java
@@ -1,4 +1,11 @@
-
+import java.util.*;
 public class Main {
-
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Type in the first coordinate point: ");
+		Point a = new Point(in.nextInt(),in.nextInt());
+		System.out.println("Type in the second coordinate point: ");
+		Point b = new Point(in.nextInt(),in.nextInt());
+		System.out.println("Distance: " + a.distance(b));
+	}
 }
