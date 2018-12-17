7ab98d0132f5c5ad130321b12e2f2d11b3ff46bc
Luke Tseng <lukegtseng@gmail.com>
2018-12-17 10:22:31 -0800

    Update Main.java

--- HelloGithub/src/Main.java
+++ HelloGithub/src/Main.java
@@ -1,11 +1,13 @@
 import java.util.*;
 public class Main {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+		Scanner s = new Scanner(System.in);
 		System.out.println("Type in the first coordinate point: ");
-		Point a = new Point(in.nextInt(),in.nextInt());
+		Point a = new Point(s.nextInt(),s.nextInt());
 		System.out.println("Type in the second coordinate point: ");
-		Point b = new Point(in.nextInt(),in.nextInt());
-		System.out.println("Distance: " + a.distance(b));
+		Point b = new Point(s.nextInt(),s.nextInt());
+		System.out.println(a.polarCoord());
+		System.out.println(b.polarCoord());
+		System.out.println(Point.distance(a,b));
 	}
 }
