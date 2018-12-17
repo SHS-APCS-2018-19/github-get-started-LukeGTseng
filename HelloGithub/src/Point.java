9e10229260afc4bf1bbf8f9ed86a84dc6575ec9e
Luke Tseng <lukegtseng@gmail.com>
2018-12-17 10:22:50 -0800

    Update Point.java

--- HelloGithub/src/Point.java
+++ HelloGithub/src/Point.java
@@ -1,13 +1,27 @@
 public class Point {
 	private int x;
 	private int y;
+	private double radius;
+	private double theta;
+	
 	public Point(int x, int y) {
 		this.x = x;
 		this.y = y;
 		System.out.printf("Coordinates: %s\n", getCoord());
 	}
-	public double distance(Point moo) { //formula: square root of (x2-x1) squared + (y2-y1) squared;
-		double distance = Math.sqrt(Math.pow(moo.x-this.x,2)+ Math.pow(moo.y-this.y,2));
+	public String polarCoord() {
+		return String.format("Polar Coordinate: (%.5f, %.5f)", getRadius(getX(), getY()), getTheta(getY(), getX()));
+	}
+	public double getRadius(int x, int y) {
+		radius = Math.sqrt(Math.pow(x,2) + Math.pow(y, 2));
+		return radius;
+	}
+	public double getTheta(int y, int x) {
+		theta = Math.atan2(y,x);
+		return theta;
+	}
+	public static double distance(Point moo, Point dab) { //formula: square root of (x2-x1) squared + (y2-y1) squared;
+		double distance = Math.sqrt(Math.pow(moo.x-dab.x,2)+ Math.pow(moo.y-dab.y,2));
 		return distance;
 	}
 	public int getX() {
