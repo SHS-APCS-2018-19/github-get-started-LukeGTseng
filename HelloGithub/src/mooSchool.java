d7f4eeef5af8efef3c9b8cfc83057f95bd03bc70
Luke Tseng <lukegtseng@gmail.com>
2019-01-07 08:44:36 -0800

    Pog

--- HelloGithub/src/mooSchool.java
+++ HelloGithub/src/mooSchool.java
@@ -7,8 +7,10 @@ public class mooSchool {
 		School[] totalClasses = null;
 		ArrayList<School> list = new ArrayList<School>();
 		for (;;) {
-			System.out.println(
-					"Type \"1\" to create a new class\nType \"2\" to add a student to the class\nType \"3\" to get details about all the classes\nType \"4\" to get details on a specific class\nType \"5\" to list total amount of classes");
+			System.out.println("Type \"1\" to create a new class" + "\nType \"2\" to add a student to the class"
+					+ "\nType \"3\" to get details about all the classes"
+					+ "\nType \"4\" to get details on a specific class" + "\nType \"5\" to list total amount of classes"
+					+ "\nType \"6\" to check what classes a specific student is taking");
 			int num = s.nextInt();
 			if (num == 1) {
 				list.add(School.readFromInput());
@@ -26,7 +28,8 @@ public class mooSchool {
 					System.out.println();
 					Scanner input = new Scanner(System.in);
 					String selectedClass = input.nextLine();
-					for (int i = 0; i < totalClasses.length; i++) { // another for loop to test which class the user picked
+					for (int i = 0; i < totalClasses.length; i++) { // another for loop to test which class the user
+																	// picked
 						if (selectedClass.equals(totalClasses[i].getCourseName())) {
 							totalClasses[i].addStudent(totalClasses[i]);
 							courseFound = true;
@@ -57,7 +60,8 @@ public class mooSchool {
 					System.out.println();
 					Scanner input = new Scanner(System.in);
 					String selectedClass = input.nextLine();
-					for (int i = 0; i < totalClasses.length; i++) { // another for loop to test which class the user picked
+					for (int i = 0; i < totalClasses.length; i++) { // another for loop to test which class the user
+																	// picked
 						if (selectedClass.equals(totalClasses[i].getCourseName())) {
 							totalClasses[i].printClassFormat(totalClasses[i]);
 							courseFound = true;
@@ -69,7 +73,48 @@ public class mooSchool {
 				}
 			} else if (num == 5) {
 				System.out.printf("Number of Classes: %d\n", School.getTotalCourses());
+			} else if (num == 6) {
+				String classList = "This student is in classes: ";
+				Boolean studentFound = false;
+				System.out.print("Type which student you want details on: ");
+				for (int i = 0; i < School.getAllStudents().length; i++) { // for loop to get every student made
+					System.out.print("\n" + School.getAllStudents()[i]);
+				}
+				System.out.println();
+				Scanner input = new Scanner(System.in);
+				String selectedStudent = input.nextLine();
+				for (int i = 0; i < School.getAllStudents().length; i++) { // another for loop to test which student the
+																			// user picked
+					if (selectedStudent.equals(School.getAllStudents()[i])) {
+						for (int j = 0; j < totalClasses.length; j++) {
+							if (totalClasses[j].getStudents() != null) {
+								if (findStudent(totalClasses[j].getStudents(), School.getAllStudents()[i]) == true) {
+									classList += totalClasses[j].getCourseName();
+									classList += ", ";
+								}
+							}
+						}
+					}
+					studentFound = true;
+				}
+				if (!classList.equals("This student is in classes: ")) {
+					classList = classList.substring(0, classList.length() - 2);
+					System.out.println(classList);
+				} else {
+					System.out.println("This student is not in any classes!!!");
+				}
+				if (studentFound == false) { // if the student is not found, this if statement will run
+					System.out.println("Student not found!!!");
+				}
 			}
 		}
+	}
+
+	public static boolean findStudent(String[] arr, String student) {
+		for (String s : arr) {
+			if (s.equals(student))
+				return true;
+		}
+		return false;
 	}
 }
