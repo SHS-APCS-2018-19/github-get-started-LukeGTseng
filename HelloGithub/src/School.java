0f098bca63b254f69d7f1137753951f9efbf1f54
LukeGTseng <lukegtseng@gmail.com>
2019-01-06 21:39:43 -0800

    Quick Brain Cell loss
    Pog Hehexd

--- HelloGithub/src/School.java
+++ HelloGithub/src/School.java
@@ -1,16 +1,18 @@
 import java.util.*;
 
 public class School {
+	// field for all variables
 	private String courseName;
 	private String teacherName;
 	private int roomNumber;
 	private int period;
+	private static int numOfCourses;
 	private final static int MAX_CLASS_SIZE = 30;
-	private static String[] students;
+	private String[] students;
 	ArrayList<String> list = new ArrayList<String>();
 	Scanner s = new Scanner(System.in);
 
-	public static School readFromInput() {
+	public static School readFromInput() { // class information input method
 		School newSchool = new School();
 		System.out.println("Enter the Course Name: ");
 		newSchool.addCourse();
@@ -20,10 +22,11 @@ public class School {
 		newSchool.addRoom();
 		System.out.println("Enter the Period: ");
 		newSchool.addPeriod();
-
+		numOfCourses++;
 		return newSchool;
 	}
 
+	// input methods for everything
 	public void addCourse() {
 		courseName = s.nextLine();
 	}
@@ -40,19 +43,62 @@ public class School {
 		period = s.nextInt();
 	}
 
-	public void addStudent() {
-		if (isClassFull() == false) {
-			list.add(s.nextLine());
-			students = list.toArray(new String[list.size()]);
+	public void addStudent(School school) {
+		Scanner studentNameInput = new Scanner(System.in);
+		if (isClassFull(school) == false) { // checking if class is full by using the method that checks the amount of students
+			System.out.println("Enter Student's Name: ");
+			list.add(studentNameInput.nextLine());
+			students = list.toArray(new String[list.size()]); // shout out to IMLame for the help
 		}
 
 	}
 
-	public static boolean isClassFull() {
-		if (students == null) {
-			return false;
+	public static int getTotalCourses() {
+		return numOfCourses; // used for function 4 which prints out the total number of classes made
+	}
 
-		} else if (students != null && students.length < MAX_CLASS_SIZE) {
+	public String getCourseName() {
+		return courseName; // used to find classes by name
+	}
+
+	public String getTeacher() {
+		return teacherName; // used to find the teacher name
+	}
+
+	public int getRoom() {
+		return roomNumber; // used to find the room number
+	}
+
+	public int getPeriod() {
+		return period; // used to find the period
+	}
+
+	public String[] getStudents() {
+		return students; // used to find the period
+	}
+
+	public void printClassFormat(School schools) { // format for printing out class details
+
+		String studentNames = "";
+		if (schools.getStudents() != null) {
+			for (int i = 0; i < schools.getStudents().length; i++) {
+				studentNames += schools.getStudents()[i];
+				studentNames += ", ";
+			}
+			studentNames = studentNames.substring(0, studentNames.length() - 2);
+		}
+		if (studentNames.equals("")) {
+			studentNames = "None";
+		}
+		System.out.printf("Course: %s\nTeacher: %s\nRoom: %d\nPeriod:%d\nStudents: %s\n", schools.getCourseName(),
+				schools.getTeacher(), schools.getRoom(), schools.getPeriod(), studentNames);
+
+	}
+
+	public static boolean isClassFull(School school) {
+		if (school.getStudents() == null) { // checking if array is empty
+			return false;
+		} else if (school.getStudents() != null && school.getStudents().length < MAX_CLASS_SIZE) { // testing if its in class size range
 			return false;
 		} else {
 			System.out.println("Class Full!");
--- HelloGithub/src/mooSchool.java
+++ HelloGithub/src/mooSchool.java
@@ -1,20 +1,74 @@
 import java.util.*;
+
 public class mooSchool {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		School[] totalClasses = null;
-		ArrayList<String> list = new ArrayList<String>();
-		for(;;) {
-			System.out.println("Type \"1\" to create a new class\nType \"2\" to add a student to the class\nType \"3\" to get details about all the classes\\nType \\\"3\\\" to list total amount of classes");
+		ArrayList<School> list = new ArrayList<School>();
+		for (;;) {
+			System.out.println(
+					"Type \"1\" to create a new class\nType \"2\" to add a student to the class\nType \"3\" to get details about all the classes\nType \"4\" to get details on a specific class\nType \"5\" to list total amount of classes");
 			int num = s.nextInt();
-			if(num == 1) {
+			if (num == 1) {
 				list.add(School.readFromInput());
-				totalClasses = list.toArray(new School[list.size()]);
-			} else if(num == 2) {
-				
-			} else if(num == 3) {
-				
+				totalClasses = list.toArray(new School[list.size()]); // got some help with example code
+
+			} else if (num == 2) {
+				if (totalClasses == null) {
+					System.out.println("You haven't created any classes yet!!!");
+				} else {
+					Boolean courseFound = false;
+					System.out.print("Type which class you want to add a student in: ");
+					for (int i = 0; i < totalClasses.length; i++) { // for loop to get every class made
+						System.out.print("\n" + totalClasses[i].getCourseName());
+					}
+					System.out.println();
+					Scanner input = new Scanner(System.in);
+					String selectedClass = input.nextLine();
+					for (int i = 0; i < totalClasses.length; i++) { // another for loop to test which class the user picked
+						if (selectedClass.equals(totalClasses[i].getCourseName())) {
+							totalClasses[i].addStudent(totalClasses[i]);
+							courseFound = true;
+						}
+					}
+					if (courseFound == false) { // if the class is not found, this if statement will run
+						System.out.println("Class not found!!!");
+					}
+				}
+			} else if (num == 3) {
+				if (totalClasses == null) {
+					System.out.println("You have not created any classes yet!!!");
+				} else {
+					for (int i = 0; i < totalClasses.length; i++) { // quick for loop to print out every class
+						totalClasses[i].printClassFormat(totalClasses[i]);
+						System.out.println();
+					}
+				}
+			} else if (num == 4) {
+				if (totalClasses == null) {
+					System.out.println("You have not created any classes yet!!!");
+				} else {
+					Boolean courseFound = false;
+					System.out.print("Type which class you want details on: ");
+					for (int i = 0; i < totalClasses.length; i++) { // for loop to get every class made
+						System.out.print("\n" + totalClasses[i].getCourseName());
+					}
+					System.out.println();
+					Scanner input = new Scanner(System.in);
+					String selectedClass = input.nextLine();
+					for (int i = 0; i < totalClasses.length; i++) { // another for loop to test which class the user picked
+						if (selectedClass.equals(totalClasses[i].getCourseName())) {
+							totalClasses[i].printClassFormat(totalClasses[i]);
+							courseFound = true;
+						}
+					}
+					if (courseFound == false) { // if the class is not found, this if statement will run
+						System.out.println("Class not found!!!");
+					}
+				}
+			} else if (num == 5) {
+				System.out.printf("Number of Classes: %d\n", School.getTotalCourses());
 			}
 		}
 	}
