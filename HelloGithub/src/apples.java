0bed4dabcba7c227a33df5827398aa369325a31e
LukeGTseng <lukegtseng@gmail.com>
2019-01-06 22:38:12 -0800

    POGGERS
    exercise 9 pog

--- HelloGithub/src/apples.java
+++ HelloGithub/src/apples.java
@@ -1,36 +1,78 @@
+import java.util.*;
+
 public class apples {
 	private String name;
 	private String address;
 	private String city;
+	private String state;
 	private int day;
 	private int month;
 	private int year;
 	private static int addresses = 0;
-	
-	public apples(String name, String address, String city, int month, int day, int year) {
-		this.name = name;
-		this.address = address;
+	Scanner s = new Scanner(System.in);
+
+	public static apples readFromInput() { // class information input method
+		apples newAddress = new apples();
+		System.out.println("Enter Person's Name: ");
+		newAddress.addName();
+		System.out.println("Enter Person's Address: ");
+		newAddress.addAddress();
+		System.out.println("City?");
+		newAddress.addCity();
+		System.out.println("State?");
+		newAddress.addState();
+		System.out.println("Enter Person's Birthday: ");
+		newAddress.addBirthday();
 		addresses++;
-		this.city = city;
-		this.day = day;
-		this.month = month;
-		this.year = year;
-		System.out.printf("The person's name is %s\n", getName());
-		System.out.printf("%s's address is %s\n", getName(), getAddress());
-		System.out.printf("%s's birthday is %s\n", getName(), getBirthday());
-		System.out.printf("Addresses: %d\n", getAddresses());
-		
+		return newAddress;
 	}
-	public String getBirthday() {
+
+	// input methods for everything
+	public void addName() {
+		name = s.nextLine();
+	}
+
+	public void addAddress() {
+		address = s.nextLine();
+	}
+
+	public void addCity() {
+		city = s.nextLine();
+	}
+
+	public void addState() {
+		state = s.nextLine();
+	}
+
+	public void addBirthday() {
+		System.out.println("Month: ");
+		month = s.nextInt();
+		System.out.println("Day: ");
+		day = s.nextInt();
+		System.out.println("Year: ");
+		year = s.nextInt();
+	}
+
+	// formatting methods
+	public String birthdayFormat() {
 		return String.format("%d/%d/%d", month, day, year);
 	}
+
+
+	public String addressFormat() {
+		return String.format("%s, %s, %s", address, city, state);
+	}
+
+	public static int getAddresses() {
+		return addresses;
+	}
+
 	public String getName() {
 		return name;
 	}
-	public String getAddress() {
-		return String.format("%s, %s", address, city);
-	}
-	public static int getAddresses() {
-		return addresses;
+
+	public void printAddressFormat(apples apples) {
+		System.out.printf("Name: %s\nAddress: %s\nBirtyday: %s\n", apples.getName(), apples.addressFormat(), apples.birthdayFormat());
+
 	}
 }
--- HelloGithub/src/tuna.java
+++ HelloGithub/src/tuna.java
@@ -1,10 +1,50 @@
+import java.util.*;
 class tuna {
 	public static void main(String[] args) {
-		apples person1 = new apples("Luke", "100 140th Ave SE", "Bellevue, WA", 6, 25, 2003);
-		apples person2 = new apples("Sammamish High School", "100 140th Ave SE", "Bellevue, WA", 11, 5, 1974);
-		System.out.println("There are " + apples.getAddresses() + " addresses");
-		/*System.out.println("The person's name is " + person1.getName());
-		System.out.println(person1.getName() + "'s address is " + person1.getAddress());
-		System.out.println(person1.getName() + "'s birthday is " + person1.getBirthday());*/
+		Scanner s = new Scanner(System.in);
+		apples[] totalAddresses = null;
+		ArrayList<apples> list = new ArrayList<apples>();
+		for (;;) {
+			System.out.println(
+					"Type \"1\" to create a new address\nType \"2\" to get details about all people\nType \"3\" to get details on a specific person\nType \"4\" to list total amount of people in the address book");
+			int num = s.nextInt();
+			if (num == 1) {
+				list.add(apples.readFromInput());
+				totalAddresses = list.toArray(new apples[list.size()]);
+			} else if (num == 2) {
+				if (totalAddresses == null) {
+					System.out.println("You have not created any classes yet!!!");
+				} else {
+					for (int i = 0; i < totalAddresses.length; i++) {
+						totalAddresses[i].printAddressFormat(totalAddresses[i]);
+						System.out.println();
+					}
+				}
+			} else if (num == 3) {
+				if (totalAddresses == null) {
+					System.out.println("You have not created any addresses yet!!!");
+				} else {
+					Boolean addressFound = false;
+					System.out.print("Type which person you want details on: ");
+					for (int i = 0; i < totalAddresses.length; i++) {
+						System.out.print("\n" + totalAddresses[i].getName());
+					}
+					System.out.println();
+					Scanner input = new Scanner(System.in);
+					String selectedClass = input.nextLine();
+					for (int i = 0; i < totalAddresses.length; i++) {
+						if (selectedClass.equals(totalAddresses[i].getName())) {
+							totalAddresses[i].printAddressFormat(totalAddresses[i]);
+							addressFound = true;
+						}
+					}
+					if (addressFound == false) {
+						System.out.println("Address not found!!!");
+					}
+				}
+			} else if (num == 4) {
+				System.out.printf("Number of People in Address Book: %d\n", apples.getAddresses());
+			}
+		}
 	}
 }
