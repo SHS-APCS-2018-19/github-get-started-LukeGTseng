397a4c4250459f99fc11a885717821709d58bdaa
Luke Tseng <lukegtseng@gmail.com>
2019-01-09 09:28:14 -0800

    Update Vehicle.java

--- HelloGithub/src/Vehicle.java
+++ HelloGithub/src/Vehicle.java
@@ -2,20 +2,28 @@ import java.util.*;
 public class Vehicle {
 	private Boolean onOff = false;
 	private int speed;
+	public static int numOfVehicles = 0;
 	Scanner s = new Scanner(System.in);
 
+	public static Vehicle readFromInput() {
+		Vehicle newVehicle = new Vehicle();
+		numOfVehicles++;
+		return newVehicle;
+	}
 	public void start() {
 		onOff = true;
-		System.out.println("Starting car");
+		System.out.println("Starting vehicle");
+		System.out.println("Set the speed of the vehicle");
+		setSpeed(s.nextInt());
 	}
 	public void stop() {
 		onOff = false;
-		System.out.println("Stopping car");
+		this.speed = 0;
+		System.out.println("Stopping vehicle");
 	}
-	public void setSpeed() {
-		System.out.println("Set Speed: ");
-		speed = s.nextInt();
-		System.out.println("Speed set to " + speed + " mph");
+	public void setSpeed(int speed) {
+		this.speed = speed;
+		System.out.println("Speed set to " + this.speed + " mph");
 	}
 	public void turn() {
 		String input;
@@ -30,12 +38,15 @@ public class Vehicle {
 	public int getSpeed() {
 		return speed;
 	}
-	public void increaseSpeed() {
-		speed += 5;
+	public void increaseSpeed(int s) {
+		speed += s;
 		System.out.printf("Speed now at %d mph\n", speed);
 	}
-	public void decreaseSpeed() {
-		speed -= 5;
+	public void decreaseSpeed(int s) {
+		speed -= s;
 		System.out.printf("Speed now at %d mph\n", speed);
+	}
+	public Boolean getStatus() {
+		return onOff;
 	}
 }
