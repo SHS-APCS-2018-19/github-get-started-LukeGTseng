87e46d9d3ae0b7f23f156c9a0d2f82a30235077e
Luke Tseng <lukegtseng@gmail.com>
2019-01-14 09:47:08 -0800

    Update and rename Driver to Driver.java

--- HelloGithub/src/vehicles/Driver.java
+++ HelloGithub/src/vehicles/Driver.java
@@ -20,25 +20,44 @@ public class Driver {
 					+ "Type \"3\" to stop a vehicle\n"
 					+ "Type \"4\" to turn a vehicle\n"
 					+ "Type \"5\" to increase speed of a vehicle\n"
-					+ "Type \"6\" to decrease speed of a vehicle");
+					+ "Type \"6\" to decrease speed of a vehicle\n"
+					+ "Type \"7\" to list out total number of vehicles");
 			int num = s.nextInt();
 			if(num == 1) {
 				System.out.println("Do want to create a new car or truck?");
 				String select = s.next();
 				if(select.equals("car")) {
 					list.add(Car.readFromInput());
-					totalVehicles = list.toArray(new Car[list.size()]);
+					totalVehicles = list.toArray(new Vehicle[list.size()]);
 					
 				} else if(select.equals("truck")) {
 					list.add(Truck.readFromInput());
-					totalVehicles = list.toArray(new Truck[list.size()]);
+					totalVehicles = list.toArray(new Vehicle[list.size()]);	
 				} else {
 					System.out.println("Not an option");
 				}
 			} else if (num == 2) {
+				Boolean vehicleFound = false;
+				System.out.print("Type which vehicle you want to start: ");
+				for (int i = 0; i < totalVehicles.length; i++) {
+					System.out.print("\n" + totalVehicles[i].getLicensePlate());
+				}
+				System.out.println();
+				Scanner input = new Scanner(System.in);
+				String selectedVehicle = input.nextLine();
+				for (int i = 0; i < totalVehicles.length; i++) {
+					if (selectedVehicle.equals(totalVehicles[i].getLicensePlate())) {
+						totalVehicles[i].start();
+						vehicleFound = true;
+					}
+				}
+				if (vehicleFound == false) {
+					System.out.println("Class not found!!!");
+				}
+			} else if (num == 7) {
 				System.out.println(Vehicle.numOfVehicles);
 			}
-		}
 		
+		}
 	}
 }
