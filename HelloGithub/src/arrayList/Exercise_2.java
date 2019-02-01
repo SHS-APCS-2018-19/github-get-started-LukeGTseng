a5092380de92dc31261007ec0776b90c98999df7
Luke Tseng <lukegtseng@gmail.com>
2019-02-01 09:49:27 -0800

    Update Exercise_2.java

--- HelloGithub/src/arrayList/Exercise_2.java
+++ HelloGithub/src/arrayList/Exercise_2.java
@@ -4,8 +4,11 @@ import java.util.*;
 
 public class Exercise_2 {
 	public static void main(String[] args) throws FileNotFoundException {
+		ArrayList<Employee> EmployeeList= new ArrayList<Employee>();
 		File myFile = new File("employee.txt");
 		Scanner s = new Scanner(myFile);
-		
+		while(s.hasNextLine()) {
+			Employee.readFromFile(s.nextLine());
+		}
 	}
 }
