ec36c5bd0aa88e5a216bc9ef52a57b3adcfe3f77
Luke Tseng <lukegtseng@gmail.com>
2019-02-15 11:58:47 -0800

    Update Exercise_3.java

--- HelloGithub/src/Unit_7/Exercise_3.java
+++ HelloGithub/src/Unit_7/Exercise_3.java
@@ -4,12 +4,29 @@ import java.util.*;
 import java.io.*;
 
 public class Exercise_3 {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws FileNotFoundException {
+		int[] arr = new int[20];
+		File search = new File("C:\\Users\\s-tsengl\\.eclipse\\numbers.txt");
+		Scanner s = new Scanner(search);
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = s.nextInt();
+		}
+		System.out.println(Arrays.toString(selectionSort(arr)));
 	}
-	public static ArrayList<Integer> selectionSort(ArrayList<Integer> list) {
-		
-		
-		
+
+	public static int[] selectionSort(int[] arr) {
+		int n = arr.length;
+		for (int i = 0; i < n - 1; i++) {
+			int min = i;
+			for (int j = i + 1; j < n; j++) {
+				if (arr[j] < arr[min]) {
+					min = j;
+				}
+			}
+			int temp = arr[min];
+			arr[min] = arr[i];
+			arr[i] = temp;
+		}
+		return arr;
 	}
 }
