aa58fbb9c69de76265013d35599be81957fb2e0a
Luke Tseng <lukegtseng@gmail.com>
2019-02-15 09:39:10 -0800

    Update Exercise_2.java

--- HelloGithub/src/Unit_7/Exercise_2.java
+++ HelloGithub/src/Unit_7/Exercise_2.java
@@ -5,10 +5,6 @@ import java.util.*;
 
 public class Exercise_2 {
 	public static void main(String[] args) throws FileNotFoundException {
-		int f = 0;
-		int n = 0;
-		int ft = 0;
-		int sn = 0;
 		File search = new File(
 				"C:\\Users\\s-tsengl\\Documents\\github-get-started-LukeGTseng-master\\HelloGithub\\src\\Unit_7\\numsearch.txt");
 		Scanner s = new Scanner(search);
@@ -16,30 +12,25 @@ public class Exercise_2 {
 		while (s.hasNext()) {
 			list.add(s.nextInt());
 		}
-		f = binarySearch(list,5);
-		n = binarySearch(list,9);
-		ft = binarySearch(list,43);
-		sn = binarySearch(list,79);
-		System.out.println("Five: " + f);
-		System.out.println("Nine: " + n);
-		System.out.println("Forty-Three: " + ft);
-		System.out.println("Seventy-Nine: " + sn);
+		System.out.println("Five: " + binarySearch(list,5));
+		System.out.println("Nine: " + binarySearch(list,9));
+		System.out.println("Forty-Three: " + binarySearch(list,43));
+		System.out.println("Seventy-Nine: " + binarySearch(list,79));
 	}
 
-	public static int binarySearch(ArrayList<Integer> list, int target)
-	{
+	public static int binarySearch(ArrayList<Integer> list, int target) {
 	    int min = 0;
 	    int max = list.size() - 1;
 	    
-	    while (min <= max)
-	    {
+	    while (min <= max) {
 	        int mid = (min + max) / 2;
-	        if (list.get(mid) < target)
-	            min = mid + 1;
-	        else if (list.get(mid) > target) 
+	        if (list.get(mid) < target) {
+	            min = mid + 1;  
+	        } else if (list.get(mid) > target) {
 	            max = mid - 1;
-	        else 
+	        } else {
 	            return mid;
+	        }
 	    }
 	   return -(min + 1);
 	}
