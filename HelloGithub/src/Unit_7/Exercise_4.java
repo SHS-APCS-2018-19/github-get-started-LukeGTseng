a3486879671dad16905665890148ca89e73ebeaa
Luke Tseng <lukegtseng@gmail.com>
2019-02-27 09:17:23 -0800

    Update Exercise_4.java

--- HelloGithub/src/Unit_7/Exercise_4.java
+++ HelloGithub/src/Unit_7/Exercise_4.java
@@ -16,26 +16,49 @@ public class Exercise_4 {
 		list.add("Sam");
 		String[] xd = new String[list.size()];
 		xd = list.toArray(xd);
-		insertionSort(xd);
-		System.out.println(Arrays.toString(xd));
+		String[] empty = new String[0];
+		String[] one = new String[1];
+		String[] same = {"Sam", "Sam", "Sam"};
+		if (checkValid(xd) == true) {
+			insertionSort(xd);
+			System.out.println(Arrays.toString(xd));
+		}
+		if (checkValid(empty) == true) {
+			insertionSort(empty);
+			System.out.println(Arrays.toString(empty));
+		}
+		if (checkValid(one) == true) {
+			insertionSort(one);
+			System.out.println(Arrays.toString(one));
+		}
+		if (checkValid(same) == true) {
+			insertionSort(same);
+			System.out.println(Arrays.toString(same));
+		}
 	}
 
 	public static void insertionSort(String[] xd) {
+		for (int i = 1; i < xd.length; i++) {
+			for (int j = i; j > 0 && xd[j].compareTo(xd[j - 1]) < 0; j--) {
+				String temp = xd[j];
+				xd[j] = xd[j - 1];
+				xd[j - 1] = temp;
+			}
+		}
+	}
+
+	public static boolean checkValid(String[] xd) {
 		if (xd.length == 0) {
-			System.out.println("xd is empty");
+			System.out.println("Array is empty");
+			return false;
 		} else if (xd.length == 1) {
-			
-			System.out.println("There is only 1 element in the xd");
+			System.out.println("There is only 1 element in the array");
+			return false;
 		} else if (check(xd) == true) {
 			System.out.println("All elements are the same");
+			return false;
 		} else {
-			for (int i = 1; i < xd.length; i++) {
-				for (int j = i; j > 0 && xd[j].compareTo(xd[j - 1]) < 0; j--) {
-					String temp = xd[j];
-					xd[j] = xd[j - 1];
-					xd[j - 1] = temp;
-				}
-			}
+			return true;
 		}
 	}
 
@@ -46,7 +69,25 @@ public class Exercise_4 {
 			}
 		}
 		return true;
+	}
 
+	public static void reverse(String[] xd) {
+		if (xd.length == 0) {
+			System.out.println("xd is empty");
+		} else if (xd.length == 1) {
+
+			System.out.println("There is only 1 element in the xd");
+		} else if (check(xd) == true) {
+			System.out.println("All elements are the same");
+		} else {
+			for (int i = 1; i < xd.length; i++) {
+				for (int j = i; j > 0 && xd[j].compareTo(xd[j - 1]) > 0; j--) {
+					String temp = xd[j];
+					xd[j] = xd[j - 1];
+					xd[j - 1] = temp;
+				}
+			}
+		}
 	}
 
 }
