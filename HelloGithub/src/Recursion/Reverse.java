030745d3986dcb411f34e79f9825d1914e7b3ec6
Luke Tseng <lukegtseng@gmail.com>
2019-03-01 10:39:54 -0800

    Update and rename Exercise_1.java to Reverse.java

--- HelloGithub/src/Recursion/Reverse.java
+++ HelloGithub/src/Recursion/Reverse.java
@@ -1,7 +1,7 @@
 package Recursion;
 import java.util.*;
 
-public class Exercise_1 {
+public class Reverse {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		String str = s.nextLine();
