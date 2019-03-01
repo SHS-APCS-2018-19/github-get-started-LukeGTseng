9dfb3020ac13b68ef196ddcd01e23abf873e96bc
Luke Tseng <lukegtseng@gmail.com>
2019-03-01 12:59:29 -0800

    Create GCD.java

---
+++ HelloGithub/src/Recursion/GCD.java
@@ -0,0 +1,25 @@
+package Recursion;
+import java.util.*;
+
+public class GCD {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int num1 = s.nextInt();
+		int num2= s.nextInt();
+		System.out.println(findGCD(num1,num2));
+	}
+	
+	public static int findGCD(int num1, int num2) {
+		if(num1 < 0) {
+			num1 *= -1;
+		}
+		if(num2 < 0) {
+			num2 *= -1;
+		}
+		if (num2 == 0) {
+			return num1;
+		}
+		return findGCD(num2, num1 % num2);
+	}
+	
+}
