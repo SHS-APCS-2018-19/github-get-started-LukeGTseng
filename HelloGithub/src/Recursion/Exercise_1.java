e0ec927a3bc120d4d57e590ed6b76caf39241e09
Luke Tseng <lukegtseng@gmail.com>
2019-03-01 10:32:32 -0800

    Create Exercise_1.java

---
+++ HelloGithub/src/Recursion/Exercise_1.java
@@ -0,0 +1,24 @@
+package Recursion;
+import java.util.*;
+
+public class Exercise_1 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String str = s.nextLine();
+		String rev = reverse(str);
+		System.out.println(rev);
+		if(rev.toLowerCase().equals(str.toLowerCase())) {
+			System.out.println("Your input is a palindrome");
+		} else {
+			System.out.println("Your input is not a palindrome");
+		}
+	}
+	
+	public static String reverse(String str) {
+		if(str.length()<=1 || str== null) {
+			return str;
+		} else {
+			return reverse(str.substring(1)) + str.charAt(0);
+		}
+	}
+}
