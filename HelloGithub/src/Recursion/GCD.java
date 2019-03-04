3eed476637a46171c90b1e922f8ca453bac31c48
Luke Tseng <lukegtseng@gmail.com>
2019-03-04 09:44:38 -0800

    Update GCD.java

--- HelloGithub/src/Recursion/GCD.java
+++ HelloGithub/src/Recursion/GCD.java
@@ -10,12 +10,6 @@ public class GCD {
 	}
 	
 	public static int findGCD(int num1, int num2) {
-		if(num1 < 0) {
-			num1 *= -1;
-		}
-		if(num2 < 0) {
-			num2 *= -1;
-		}
 		if (num2 == 0) {
 			return num1;
 		}
